207f73bd18d7b62568bec5654a63d5193ccfb283
Bryan Cordova Ñ <54011124+BryanCordova@users.noreply.github.com>
2022-04-27 22:00:15 -0500

    Update activate.bat

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Nicole Jane Caceres\4 ciclo\2 Desarrollo de Aplicaciones Empresariales\Archivos\Lab07\djangoApp07\myvenv
+set VIRTUAL_ENV=C:bryan\Lab07\djangoApp07\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
